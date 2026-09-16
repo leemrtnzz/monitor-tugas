9ed5bdaf9a25524f64060cc647084abe29a0d9e9
tuyul69 <tuyull.inet@gmail.com>
2026-09-16 20:30:23 +0700

    fix zoom inputan

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,8 @@ export const viewport: Viewport = {
   colorScheme: "dark",
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,     
+  userScalable: false,  
   viewportFit: "cover",
 };
 
