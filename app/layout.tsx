1b1914567ab078ad283fd984f6f021f985024dde
tuyul69 <tuyull.inet@gmail.com>
2026-09-22 19:46:47 +0700

    deleted favicon vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ export const metadata: Metadata = {
       { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
+    shortcut: "/icons/icon-512.png",
     apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   openGraph: {
